d7924e18a243ea58808a65c57cd09aa3ca496dc0
Hugo Recolet <hugorecolet@gmail.com>
2024-09-26 16:38:14 +0200

    [UP] GOOD

---
+++ auto_install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install qemu-system libvirt-daemon-system qemu-system-gui mtools xorriso
+sudo apt install texinfo
+sudo apt install grub-common
+sudo apt install grub-pc-bin
