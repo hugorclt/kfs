664ef12de164b0a884f9259d1c224c3651bae9d4
Adrian <adrian.mael.jung@gmail.com>
2024-09-30 16:06:09 +0200

    WIP gdt

--- srcs/gdt/gdt.h
+++ srcs/gdt/gdt.h
@@ -4,5 +4,16 @@
 
 typedef struct s_gdtr {
 	uint16_t	size;
-	uint32_t	offset;
+	uint32_t	gdt;
 }	t_gdtr __attribute__((packed));
+
+
+// union test
+// {
+// 	struct elem1 {
+// 		uint8_t	elemt1_1 : 1;
+// 		uint8_t	elem1_2 : 1;
+// 	};
+// 	uint8_t elem;
+// }
+// ;
