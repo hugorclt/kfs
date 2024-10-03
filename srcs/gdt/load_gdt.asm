6d7421e2b129691c156d3b3eefbaade188849316
Adrian <adrian.mael.jung@gmail.com>
2024-10-03 14:44:38 +0200

    good offset of stack segment

--- srcs/gdt/load_gdt.asm
+++ srcs/gdt/load_gdt.asm
@@ -3,13 +3,13 @@ global load_gdt
 load_gdt:
 	mov eax, [esp + 4]
 	lgdt [eax]
-	mov ax, 0x10
+	mov ax, 0x10	; 0x10 --> offset of data descriptor
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
+	mov ax, 0x18	; 0x18 --> offset of stack descriptor
 	mov ss, ax
-	jmp 0x08:.flush
+	jmp 0x08:.flush	; 0x08 --> offset of code descriptor
 .flush:
 	ret
-
