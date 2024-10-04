bd12af8820cbe1e3c56136a09bb9cfa3e73aa320
Adrian <adrian.mael.jung@gmail.com>
2024-10-04 14:06:24 +0200

    UP debug

--- srcs/idt/idt.c
+++ srcs/idt/idt.c
@@ -6,7 +6,7 @@ __attribute__((aligned(0x10))) // aligned for performance
 static t_idt_descriptor	idt[MAX_IDT_ENTRIES];
 t_idtr					idtr;
 
-static void	idt_init_descriptor(uint8_t i, void* handler, uint8_t flags)
+static void	idt_init_descriptor(uint8_t i, uint32_t handler, uint8_t flags)
 {
     t_idt_descriptor* descriptor = &idt[i];
 
@@ -33,7 +33,7 @@ void	exception_print()
 	printk("In: System Exeption != 13 \n");
 }
 
-void gp_handler()
+void	gp_handler()
 {
 	printk("In: General Protection Fault == 13\n");
 }
@@ -45,20 +45,20 @@ void	idt_init(void)
     for (uint8_t i = 0; i < 32; i++)
 	{
 		if (i == 13)
-        	idt_init_descriptor(i, general_p_fault_wrapper, 0x8E);
+        	idt_init_descriptor(i, (uint32_t) general_p_fault_wrapper, 0x8E);
 		else 
-        	idt_init_descriptor(i, isr_stub_table[i], 0x8E);
+        	idt_init_descriptor(i, (uint32_t) isr_stub_table[i], 0x8E);
     }
 
 	//End of system interrupt, beginning of hardware interrupt (IRQ)
 	//https://wiki.osdev.org/Interrupts
 
 	//IRQ#0 == 32 : clock
-    idt_init_descriptor(32, clock_handler_wrapper, 0x8E);
+    idt_init_descriptor(32, (uint32_t) clock_handler_wrapper, 0x8E);
 
 
 	//IRQ#1 == 33 : keyboard
-    idt_init_descriptor(33, keyboard_handler_wrapper, 0x8E);
+    idt_init_descriptor(33, (uint32_t) keyboard_handler_wrapper, 0x8E);
 
 	idtr.size =	(uint16_t) sizeof(t_idt_descriptor) * MAX_IDT_ENTRIES - 1;
 	idtr.idt =	(uintptr_t) &idt[0];
