6e59e2f1bb85c26fe4d7d99593a3ae3b48f7d9ed
hugorclt <hugorecolet@gmail.com>
2024-10-16 16:18:34 +0200

    [UP]added pseudocode mapping memory

--- srcs/memory/memory.c
+++ srcs/memory/memory.c
@@ -8,9 +8,9 @@ void print_memory_map(multiboot_info_t *bootInfo)
 		multiboot_memory_map_t *map_entry = (multiboot_memory_map_t *)(bootInfo->mmap_addr + i);
 
 		printk("%p - %p: %d \n", map_entry->addr_low, map_entry->len_low, map_entry->type);
-		// printk("address low: %x, %x\n", map_entry->addr_low, map_entry->len_low);
-		// printk("len_low: %p\n", map_entry->len_low);
-		// printk("type: %d\n", map_entry->type);
-		// printk("size: %d\n", map_entry->size);
 	}
+	unsigned int vaddr = (unsigned int) &kernel_virtual_start;
+	unsigned int vend = (unsigned int) &kernel_virtual_end;
+
+	printk("%p, %p\n", vaddr, vend);
 }
--- srcs/memory/memory.h
+++ srcs/memory/memory.h
@@ -9,8 +9,40 @@
 #include "multiboot.h"
 
 void print_memory_map(multiboot_info_t *bootInfo);
+extern void kernel_virtual_start(void);
+extern void kernel_virtual_end(void);
+extern void kernel_physical_start(void);
+extern void kernel_physical_end(void);
 
 typedef struct s_page_directory
 {
     uint32_t entries[1024];
 } t_page_directory;
+
+
+memoire_total = 32MiB
+page_frame_size = 4MiB
+
+max_page_frame = memoire_total / page_frame_size
+
+init_bitmap() {
+	bitmap[size_page_frame] = FREE
+
+	kernel_page = ( addresse_kernel - address_multiboot_start )/ page_frame_size
+	bitmap[kernel_page] = ALLOCATED
+}
+uintptr_t allocate_page_frame() {
+	for (int i =0; i < total_pf; i++) {
+		if bitmap[i] == FREE {
+			bitmap[i] = ALLOCATED 
+			return i * page_frame_size + address_multiboot_start
+		}
+		return 0
+	}
+}
+
+free_page_frame(uintptr_t addr) {
+	int i = (addr - multiboot_start) / page_frame_size;
+
+	bitmap[i] = FREE;
+}
