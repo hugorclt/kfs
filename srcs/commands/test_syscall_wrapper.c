4bc34c472118f4a4dd561b93d791aefabb116f2b
Adrian <adrian.mael.jung@gmail.com>
2024-10-29 13:29:55 +0100

    clean up

--- srcs/commands/test_syscall_wrapper.c
+++ srcs/commands/test_syscall_wrapper.c
@@ -5,6 +5,5 @@ void	test_syscall_read(int fd, void * buffer, size_t count)
 	printk("in test_syscall_read\n");
 	int ret = test_syscall_read_asm(fd, buffer, count);
 	printk("test\n");
-	printk("test\n");
-	// printk("return value of syscall_read: %d\n", ret);
+	printk("return value of syscall_read: %d\n", ret);
 }
